c7d0951c46b86f0ca66a7dd341f8dcecb4c01b99
alan-a-santos <alan-a@uol.com.br>
2024-05-05 19:34:14 -0300

    Versão 1
    Foi incluído o pesquisa de cep no cadastro do cliente

--- Projeto/routes/clientes/cadastro.py
+++ Projeto/routes/clientes/cadastro.py
@@ -1,12 +1,39 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import mysql.connector
 
 
 cadastro_route = Blueprint('cadastro', __name__)
 
 
-# @cadastro_route.route('/home1')
-# def home1():
-#     return render_template('home.html')
+@cadastro_route.route('/cliente_cadastrado', methods=['post'])
+def  cadastrar_cliente():
+    conexao = mysql.connector.connect(host='localhost', database='d_mais',user='root', password='aas798118')
+    if conexao.is_connected():
+
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        nascimento = request.form['nascimento']
+        celular = request.form['celular']
+        email = request.form['email']
+        instagram = request.form['instagram']
+        cep = request.form['cep']
+        endereco = request.form['endereco']
+        num = request.form['num']
+        bairro = request.form['bairro']
+        cidade = request.form['cidade']
+        observa = request.form['observa']
+
+        comando = (f"""INSERT INTO clientes (id,nome,cpf,nascimento,celular,email,instagram,cep,endereco,numero,bairro,cidade,observacao) VALUES (default,'{nome}','{cpf}','{nascimento}','{celular}','{email}','{instagram}','{cep}','{endereco}','{num}','{bairro}','{cidade}','{observa}');""")
+        cursor= conexao.cursor()
+        
+        cursor.execute(comando)
+        retorno = cursor.fetchall()
+        print("conectado ao banco de dados")
+    if conexao.is_connected():
+        cursor.close()
+        conexao.close()
+        print("conexao encerrada")
+    return render_template('clientes/clientes_cadastro.html')
 
 # @cadastro_route.route('/atualiza')
 # def atualizar():
