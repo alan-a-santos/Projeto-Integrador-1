595afc813d4681a9796751189f2f581a8fdf488f
alan-a-santos <alan-a@uol.com.br>
2024-05-20 16:23:03 -0300

    Versão FInal

--- Projeto/app.py
+++ Projeto/app.py
@@ -34,6 +34,5 @@ app.register_blueprint(atualizap_route)
 app.register_blueprint(excluip_route)
 
 
-if (__name__) == "(__main__)":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+#if (__name__) == "(__main__)":
+app.run(debug=True)
