c151aa06852cd920789ad9cbad1913e9e75f0a68
alan-a-santos <alan-a@uol.com.br>
2024-05-24 12:44:43 -0300

    Revert "Versão 6.1"
    This reverts commit ee345494b915bed7376cdeccd0759cc053c1f3cf.

--- Projeto/_az_config.sh
+++
@@ -1,9 +0,0 @@
-SUBSCRIPTION="Azure subscription 1"
-RESOURCEGROUP="dmaismarmitaria_group"
-LOCATION="brazilsouth"
-PLANNAME="ASP-dmaismarmitariagroup-8d66"
-PLANSKU="F1"
-SITENAME="dmaismarmitaria"
-RUNTIME="PYTHON|3.12"
-
-az webspp config set --resource-group $RESOURCEGROUP --name $SITENAME --startup-file "uvicorn app.main:app --host 0.0.0.0 --port 8000"
